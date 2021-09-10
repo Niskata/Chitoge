b883160bdbfdfd81f19465fc8991d4985a3f2cb8
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-10 17:37:47 +0530

    Add files via upload

---
+++ src/commands/Misc/Chitoge.ts
@@ -0,0 +1,41 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'chitoge',
+            description: 'Displays the info',
+            category: 'misc',
+            usage: `${client.config.prefix}chitoge`,
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        
+
+        
+        
+return void M.reply(await request.buffer('https://i.pinimg.com/736x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg'),
+MessageType.image,
+            undefined,
+            undefined,
+            `*✨Star✨* \n\n *Wishing you to have a nice day*💗`
+           
+
+)
+
+
+    }
+
+
+
+
+
+
+
+}
