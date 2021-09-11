b29229de0a11909bf408a170f89f13db1896e924
Alessandro Murru <88579269+alpha4041@users.noreply.github.com>
2021-09-11 13:51:53 +0200

    add hentai.ts

---
+++ src/commands/Weeb/hentai.ts
@@ -0,0 +1,39 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import  axios  from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'hentai',
+            description: 'Invia un hentai a caso',
+            category: 'weeb',
+            usage: `${client.config.prefix}hentai`,
+
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+
+        const rnekol = ["waifu"];
+        const neko = await axios.get('https://api.waifu.pics/nsfw/')
+
+return void M.reply(await request.buffer(neko.data.url), MessageType.image, undefined, undefined, `*🌟 Ecco a te, e non abusarne ;)*`)
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
