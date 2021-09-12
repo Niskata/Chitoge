687c9257897c225468b12d95c73c23b53170ad5e
Alessandro Murru <88579269+alpha4041@users.noreply.github.com>
2021-09-12 13:25:01 +0200

    Update hentai.ts

--- src/commands/Weeb/hentai.ts
+++ src/commands/Weeb/hentai.ts
@@ -18,7 +18,7 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-      const retries = 0;
+      retries = 0;
       const rnekol = ["waifu"];
       const rnekolc = rnekol[Math.floor(Math.random() * rnekol.length)];
       try {
